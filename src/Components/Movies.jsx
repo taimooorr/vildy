d64e6cccf6cc8b91003a7e3efa296a186c253574
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-10-08 08:25:15 +0500

    handled delete btn

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
-
+import {getMovies} from "../services/fakeMovieService";
 class Movies extends Component {
-	state = {};
+	state = {
+		movies : getMovies()
+	};
+	handleDelete = movie => { 
+		const movies = this.state.movies.filter(m => m._id !== movie._id);
+		this.setState({movies});
+	}
 	render() {
 		console.log("this.props");
 		return (
@@ -16,13 +22,13 @@ class Movies extends Component {
 				</thead>
 				<tbody>
 						{this.state.movies.map((movie) => (
-							<tr>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
+							<tr key={movie._id}>
+								<td>{ movie.title}</td>
+								<td>{ movie.genre.name}</td>
+								<td>{ movie.numberInStock}</td>
+								<td> {movie.dailyRentalRate}</td>
 							<td>
-								<button className="btn btn-danger btn-sm">
+								<button onClick={()=>this.handleDelete(movie)} className="btn btn-danger btn-sm">
 									Delete
 								</button>
 							</td>
